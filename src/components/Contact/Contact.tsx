a403aacb41fa9c1c602ab94a7a2ffc90c77aecab
Matheus Bezerra <mabezerra2@gmail.com>
2023-02-13 20:49:19 -0400

    fazendo o form

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Form from '../Form/Form';
 
-const Contact = (): JSX.Element => {
+const Contact: React.FC = () => {
+  const [emailSent, setEmailSent] = useState(false);
+
+  const handleSubmit = async (values: any) => {
+    const response = await fetch('http://localhost:3000/send', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(values),
+    });
+    setEmailSent(true);
+  };
+
   return (
-    <main>
-      Contact
-    </main>
-  )
-}
+    <div>
+      {emailSent ? (
+        <p>Email sent!</p>
+      ) : (
+        <Form onSubmit={handleSubmit} />
+      )}
+    </div>
+  );
+};
 
 export default Contact;
